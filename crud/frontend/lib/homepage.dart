c431aeba5e732ae33be501c2f503722d7dd340da
mission365 <missiondevnath901@gmail.com>
2024-12-27 12:08:51 +0600

    fetching data from api

--- crud/frontend/lib/homepage.dart
+++ crud/frontend/lib/homepage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import 'constants/api.dart';
+import 'models/todo.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -14,14 +15,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
+  List<Todo> myTodos = [];
   void fetchData() async{
     try{
       http.Response response = await http.get(Uri.parse(api));
-      // var data = response.body;
-      // json.decode(data);
-      // print(data);
-      print(response.body);
+      var data = json.decode(response.body);
+      // create a Todo object....
+      data.forEach((todo){
+        Todo t = Todo(
+            id: todo['id'],
+            title: todo['title'],
+            desc: todo['desc'],
+            date: todo['date'],
+            isDone: todo['isDone'],
+        );
+        myTodos.add(t);
+      });
+      print(myTodos.length);
+
+      // This got error......
+      // data.forEach((todo){
+      //   myTodos.add(todo);
+      // });
+      // print (myTodos.length);
+      // print(data.runtimeType);
+      // print(response.body.runtimeType);
     } catch(e){
       print(e);
     }
